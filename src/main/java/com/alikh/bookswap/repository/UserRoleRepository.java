2cf27ece2a6276d3ee802c2ab3d7788429d5df23
Ali Khadempour <ali.khadempour@outlook.com>
2025-07-21 21:39:05 +0330

    refactor(role-repo): rename UserRoleRepository to RoleRepository.

--- src/main/java/com/alikh/bookswap/repository/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alikh.bookswap.repository;
-
-import com.alikh.bookswap.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRoleRepository extends JpaRepository<Role, Integer> {
-}
