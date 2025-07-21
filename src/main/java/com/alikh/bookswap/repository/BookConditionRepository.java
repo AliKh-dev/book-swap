97844aeff58a89af3f753265aa979e4ea15de8db
Ali Khadempour <ali.khadempour@outlook.com>
2025-07-21 21:35:44 +0330

    feat(book-condition-repo): add findTopByOrderByIdDesc() with PESSIMISTIC_WRITE lock.

--- src/main/java/com/alikh/bookswap/repository/BookConditionRepository.java
+++ src/main/java/com/alikh/bookswap/repository/BookConditionRepository.java
@@ -1,9 +1,15 @@
 package com.alikh.bookswap.repository;
 
 import com.alikh.bookswap.entity.BookCondition;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BookConditionRepository extends JpaRepository<BookCondition, Integer> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<BookCondition> findTopByOrderByIdDesc();
 }
