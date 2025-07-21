99ed0e813a2e3e527f431d0966cbf1381849dfd0
Ali Khadempour <ali.khadempour@outlook.com>
2025-07-21 21:50:42 +0330

    feat(controllers): scaffold boilerplate empty controllers for all entities.

---
+++ src/main/java/com/alikh/bookswap/controller/BookConditionController.java
@@ -0,0 +1,10 @@
+package com.alikh.bookswap.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/book-conditions")
+@RequiredArgsConstructor
+public class BookConditionController {
+}
---
+++ src/main/java/com/alikh/bookswap/controller/ListingTypeController.java
@@ -0,0 +1,11 @@
+package com.alikh.bookswap.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/listing-types")
+@RequiredArgsConstructor
+public class ListingTypeController {
+}
