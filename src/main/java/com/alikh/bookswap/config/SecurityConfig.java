f7f370d1787bdcac31be3822f36e4d4d11be00a8
Ali Khadempour <ali.khadempour@outlook.com>
2025-07-22 18:23:44 +0330

    feat(security): Add PasswordEncoder bean for password hashing

--- src/main/java/com/alikh/bookswap/config/SecurityConfig.java
+++ src/main/java/com/alikh/bookswap/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @AllArgsConstructor
@@ -26,5 +28,10 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
