308f1f1983d6a99d27c6697774422f9f57ffdad1
Ali Khadempour <ali.khadempour@outlook.com>
2025-07-26 18:20:46 +0330

    feat(controller): Refactor ListingTypeController to return detailed responses for update and patch operations

--- src/main/java/com/alikh/bookswap/controller/ListingTypeController.java
+++ src/main/java/com/alikh/bookswap/controller/ListingTypeController.java
@@ -5,13 +5,7 @@ import com.alikh.bookswap.dto.listingtype.request.ListingTypePatchRequest;
 import com.alikh.bookswap.dto.listingtype.request.ListingTypeUpdateRequest;
 import com.alikh.bookswap.dto.listingtype.response.ListingTypeDetailResponse;
 import com.alikh.bookswap.dto.listingtype.response.ListingTypeSummaryResponse;
-import com.alikh.bookswap.dto.role.request.RoleCreateRequest;
-import com.alikh.bookswap.dto.role.request.RolePatchRequest;
-import com.alikh.bookswap.dto.role.request.RoleUpdateRequest;
-import com.alikh.bookswap.dto.role.response.RoleDetailResponse;
-import com.alikh.bookswap.dto.role.response.RoleSummaryResponse;
 import com.alikh.bookswap.service.contract.ListingTypeService;
-import com.alikh.bookswap.service.contract.RoleService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -48,21 +42,19 @@ public class ListingTypeController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> update(
+    public ResponseEntity<ListingTypeSummaryResponse> update(
             @PathVariable Integer id,
             @RequestBody @Valid ListingTypeUpdateRequest request
     ) {
-        service.update(id, request);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(service.update(id, request));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Void> patch(
+    public ResponseEntity<ListingTypeSummaryResponse> patch(
             @PathVariable Integer id,
             @RequestBody @Valid ListingTypePatchRequest request
     ) {
-        service.patch(id, request);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(service.patch(id, request));
     }
 
     @DeleteMapping("/{id}")
